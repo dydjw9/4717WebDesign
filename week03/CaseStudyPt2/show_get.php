29bd4bc47320c974a24b1adc0b9807ad63cd6427
dydjw9 <Angus@Anguss-MacBook-Pro.local>
2015-09-01 21:09:44 +0800

    9.1Work

---
+++ week03/CaseStudyPt2/show_get.php
@@ -0,0 +1,8 @@
+<?php
+echo"your infomation is as following <BR>";
+echo"name: ".$_GET['name']."<BR>";
+echo"email: ".$_GET['email']."<BR>";
+echo"startDate: ".$_GET['Startdate']."<BR>";
+echo"experience: ".$_GET['experience']."<BR>";
+ 
+?>
